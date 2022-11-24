0b12aac767e5b844cfc395d823d0527220449a22
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-11-24 21:52:12 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -827,5 +827,58 @@ class Solution(object):
         print(array)
         for i in range(len(array)):
             nums[i]=array[i]
+
+#!https://leetcode.com/problems/spiral-matrix/submissions/        
+class Solution(object):
+    def spiralOrder(self, matrix):
+        left, right = 0, len(matrix[0])
+        top, bottom = 0, len(matrix)
+        res=[]
+        while top<bottom and right>left:
+            
+            #get top
+            for i in range(left, right):
+                res.append(matrix[top][i])
+            top+=1
+            
+            #get right
+            for i in range(top, bottom):
+                res.append(matrix[i][right-1])
+            right-=1
+            if not (left<right and top<bottom):
+                break
+            #get bottom
+            for i in range(right-1, left-1, -1):
+                res.append(matrix[bottom-1][i])
+            bottom-=1
+            
+            #get left
+            for i in range(bottom-1, top-1, -1):
+                res.append(matrix[i][left])
+            left+=1
+            
+        return res
+
+
+#!https://leetcode.com/problems/search-a-2d-matrix-ii/submissions/
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        array=[]
+        for i in range(len(matrix)):
+            for j in matrix[i]:
+                if j==target:
+                    return True
+        return False
+        
+#!https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/submissions/
+class Solution(object):
+    def maxProfit(self, prices):
+        i=0
+        profit=0
+        while i<len(prices)-1:
+            if prices[i+1]-prices[i]>0:
+                profit+=prices[i+1]-prices[i]
+            i+=1
+        return profit
             
         
