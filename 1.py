776979ce619bd26bc5fce22733b40d4a90e68c32
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-12-03 10:13:01 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -961,6 +961,19 @@ class Solution(object):
             string=string+i+" "
         string = string[:len(string)-1]
         return string[::-1]
+
+#!https://leetcode.com/problems/count-asterisks/submissions/
+class Solution(object):
+    def countAsterisks(self, s):
+        s=s.split("|")
+        print(s)
+        count=0
+        for i in range(0,len(s),2):
+            for j in range(len(s[i])):
+                if s[i][j]=="*":
+                    count+=1
+        return count
+        
     
     
         
