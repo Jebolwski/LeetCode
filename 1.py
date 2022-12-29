0742e616fae6642418fa2d00d803688510e55eaf
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-12-29 21:05:56 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1201,3 +1201,21 @@ class Solution:
         if array1==array2[::-1]:
             return True
         return False
+
+#!https://leetcode.com/problems/invert-binary-tree/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        tmp=root.right
+        root.right=root.left
+        root.left=tmp
+        self.invertTree(root.right)
+        self.invertTree(root.left)
+        return root
