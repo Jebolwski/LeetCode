158b22c5d8bb1fdfb5b3036f7aef125385a5568c
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2023-01-11 19:19:13 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1530,5 +1530,11 @@ class Solution(object):
                 return i+1
         return len(costs)
                         
-
+#!https://leetcode.com/problems/determine-color-of-a-chessboard-square/
+class Solution(object):
+    def squareIsWhite(self, coordinates):
+        dicti={'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8}
+        if (int(dicti[coordinates[0]])+int(coordinates[1]))%2==0:
+            return False
+        return True
 #?----------
