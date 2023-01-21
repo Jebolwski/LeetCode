5fe538f91e423e1fe81c204dd43252309412f664
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2023-01-21 19:46:52 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1716,6 +1716,25 @@ class Solution(object):
                 total+=int(i)
             temp=total
         return temp
+
+#!https://leetcode.com/problems/restore-ip-addresses/ 
+class Solution(object):
+    def restoreIpAddresses(self, s):
+        arr=[]
+        if len(s)>12:
+            return []
+
+        def backtrack(i,dots,ip):
+            if dots==4 and i==len(s):
+                arr.append(ip[:len(ip)-1])
+                return 
+            
+            for j in range(i,min(i+3,len(s))):
+                if int(s[i:j+1])<256 and (i==j or s[i]!="0"):
+                    backtrack(j+1,dots+1,ip+s[i:j+1]+".")
+        
+        backtrack(0,0,"")
+        return arr
 #?----------
         
         
