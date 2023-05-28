a5ed563fd837abc9dbc47bb1694151cfcdef7fe6
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2023-05-28 17:13:08 +0300

    Add files via upload

--- 2.py
+++ 2.py
@@ -1798,4 +1798,12 @@ class Solution(object):
             else:
                 arr.append(-1)
         return arr
+
+#!https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        arr = list(collections.Counter(nums).keys())
+        arr1 = list(range(1,len(nums)+1))
+        res = []   
+        return set(arr1) - set(arr)
 # ?------------------
