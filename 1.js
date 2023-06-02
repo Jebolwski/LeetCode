7fd8b34f481960204b05720c76b0cfb84f122d64
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2023-06-02 21:26:51 +0300

    Add files via upload

--- 1.js
+++ 1.js
@@ -83,3 +83,8 @@ var once = function (fn) {
     }
   };
 };
+
+//!https://leetcode.com/problems/return-length-of-arguments-passed/
+var argumentsLength = function (...args) {
+  return args.length;
+};
