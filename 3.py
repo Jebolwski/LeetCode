b6514db72f15bdca655ee2c5018e48b14002f52e
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2023-07-29 10:39:40 +0300

    Add files via upload

--- 3.py
+++ 3.py
@@ -55,3 +55,93 @@ class Solution(object):
         if index == -1:
             return -1
         return index+1
+
+#!https://leetcode.com/problems/maximum-69-number/
+
+
+class Solution(object):
+    def maximum69Number(self, num):
+        x = 0
+        arr = list(str(num))
+        for i in range(len(arr)):
+            if arr[i] == "6":
+                arr[i] = "9"
+                break
+        return int("".join(arr))
+
+#!https://leetcode.com/problems/find-the-pivot-integer/
+
+
+class Solution(object):
+    def pivotInteger(self, n):
+        if n == 1:
+            return 1
+
+        def check(num1, num2):
+            x1, x2 = 0, 0
+            for i in range(num1+1):
+                x1 += i
+            for i in range(num1, num2+1):
+                x2 += i
+            if x1 == x2:
+                return True
+            return False
+
+        for i in range(n):
+            if check(i, n):
+                return i
+        return -1
+
+#!https://leetcode.com/problems/find-lucky-integer-in-an-array/
+
+
+class Solution(object):
+    def findLucky(self, arr):
+        listo = collections.Counter(arr).keys()
+        listo = sorted(listo)
+        listo = listo[::-1]
+        for i in listo:
+            if arr.count(i) == i:
+                return i
+        return -1
+
+#!https://leetcode.com/problems/convert-integer-to-the-sum-of-two-no-zero-integers/
+
+
+class Solution(object):
+    def getNoZeroIntegers(self, n):
+        if n == 2:
+            return [1, 1]
+        else:
+            for i in range(n):
+                if "0" not in str(i) and "0" not in str(n-i):
+                    return [i, n-i]
+
+
+#!https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+class Solution(object):
+    def numberOfSteps(self, num):
+        count = 0
+        while num > 0:
+            if num % 2 == 0:
+                num /= 2
+            else:
+                num -= 1
+            count += 1
+        return count
+
+#!https://leetcode.com/problems/count-operations-to-obtain-zero/
+
+
+class Solution(object):
+    def countOperations(self, num1, num2):
+        count = 0
+        while num1 != 0 and num2 != 0:
+            if num1 == num2:
+                return count+1
+            elif max(num1, num2) == num2:
+                num2 = num2-num1
+            elif max(num1, num2) == num1:
+                num1 = num1-num2
+            count += 1
+        return 0
