dbbadcfd563e5c7e6a982d948d07152b11a01a47
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2023-09-15 11:50:40 +0300

    Add files via upload

--- 3.py
+++ 3.py
@@ -1246,4 +1246,14 @@ class Solution(object):
         return True
       return len(max(arr1))-len(max(arr0))>0
 
-        
+#!https://leetcode.com/problems/replace-all-digits-with-characters/
+class Solution(object):
+    def replaceDigits(self, s):
+        alphabet=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        string=""
+        for i in range(len(s)):
+          if s[i] not in alphabet:
+            string+=alphabet[alphabet.index(s[i-1])+int(s[i])]
+          else:
+            string+=s[i]
+        return (string)
