54356a85690616d35f47ed21fa96293655a0b09c
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2023-09-26 15:50:56 +0300

    Add files via upload

--- 1.sql
+++ 1.sql
@@ -118,3 +118,11 @@ from
 queries
 group by
 query_name;
+
+--!https://leetcode.com/problems/average-selling-price/
+SELECT p.product_id, IFNULL(round(SUM(p.price*u.units)/sum(u.units),2),0) as average_price
+FROM Prices p 
+LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id AND 
+u.purchase_date BETWEEN p.Start_date and p.end_date
+GROUP BY p.product_id
