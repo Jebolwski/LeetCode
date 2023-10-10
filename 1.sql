f82983769558afd53a6eadcd43855c18d5fb981d
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2023-10-10 22:02:09 +0300

    Add files via upload

--- 1.sql
+++ 1.sql
@@ -328,3 +328,14 @@ WHERE (product_id, year) in (
     FROM Sales
     GROUP BY product_id
 )
+
+--!https://leetcode.com/problems/game-play-analysis-iv/
+SELECT
+  ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
+FROM
+  Activity
+WHERE
+  (player_id, DATE_SUB(event_date, INTERVAL 1 DAY))
+  IN (
+    SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id
+  )
