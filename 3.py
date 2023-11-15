cd796efdc782973bf7477d0be261e84358c22554
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2023-11-15 17:46:21 +0300

    Add files via upload

--- 3.py
+++ 3.py
@@ -1397,6 +1397,16 @@ class Solution(object):
                     tmp.append(i)
             total+=len(tmp)
         return (total)
+
+#!https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging
+class Solution(object):
+    def maximumElementAfterDecrementingAndRearranging(self, arr):
+        arr=sorted(arr)
+        arr[0]=1
+        for i in range(len(arr)-1):
+            if arr[i+1]-arr[i]>1:
+                arr[i+1]=arr[i]+1
+        return arr[-1]
             
         
         
