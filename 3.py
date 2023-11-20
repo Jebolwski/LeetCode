fcd85842ca6fcc99906a975af10bc47b866fdc2c
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2023-11-20 22:19:03 +0300

    Add files via upload

--- 3.py
+++ 3.py
@@ -1438,4 +1438,34 @@ class Solution(object):
                 return i
 
         
-        
+#!https://leetcode.com/problems/minimum-amount-of-time-to-collect-garbage/
+class Solution(object):
+    def garbageCollection(self, garbage, travel):
+        n = len(garbage)
+        ans = 0
+
+        for i in range(n - 1):
+            ans += 3 * travel[i]
+
+        for s in garbage:
+            ans += len(s)
+
+        for i in range(n - 1, 0, -1):
+            if "G" not in garbage[i]:
+                ans -= travel[i - 1]
+            else:
+                break
+
+        for i in range(n - 1, 0, -1):
+            if "P" not in garbage[i]:
+                ans -= travel[i - 1]
+            else:
+                break
+
+        for i in range(n - 1, 0, -1):
+            if "M" not in garbage[i]:
+                ans -= travel[i - 1]
+            else:
+                break
+
+        return ans
