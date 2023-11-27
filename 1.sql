f8e96b0b17271682f80bacd42a79fde073ac2be3
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2023-11-27 21:06:30 +0300

    Add files via upload

--- 1.sql
+++ 1.sql
@@ -370,3 +370,15 @@ GROUP BY q1.turn
 HAVING SUM(q2.weight) <= 1000
 ORDER BY SUM(q2.weight) DESC
 LIMIT 1
+
+--!https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+select * from CITY where population>100000 and countrycode='USA'
+
+--!https://www.hackerrank.com/challenges/revising-the-select-query-2/
+select name from city  where countrycode='USA' and population>120000
+
+--!https://www.hackerrank.com/challenges/select-all-sql/
+select * from city
+
+--!https://www.hackerrank.com/challenges/select-by-id/
+select * from city where id=1661
