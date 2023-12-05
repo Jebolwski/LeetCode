626b1f36253da542d920d9b473c670272310105e
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2023-12-05 23:03:22 +0300

    Add files via upload

--- 3.py
+++ 3.py
@@ -1469,3 +1469,16 @@ class Solution(object):
                 break
 
         return ans
+    
+#!https://leetcode.com/problems/minimum-time-visiting-all-points/
+class Solution(object):
+    def toTime(self, from_point, to_point):
+            x_diff = abs(from_point[0] - to_point[0])
+            y_diff = abs(from_point[1] - to_point[1])
+            return max(x_diff, y_diff)
+    def minTimeToVisitAllPoints(self, points):
+        time = 0
+        for i in range(1, len(points)):
+            print(self.toTime(points[i - 1], points[i]))
+            time += self.toTime(points[i - 1], points[i])
+        return time
