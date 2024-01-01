b79afaefc59004c5731b283b1a7cbd74c5a3e21a
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2024-01-01 14:12:32 +0300

    Add files via upload

--- 3.py
+++ 3.py
@@ -1627,4 +1627,23 @@ class Solution(object):
             return max(arr)
         return -1
         
+#!https://leetcode.com/problems/assign-cookies/
+class Solution(object):
+    def findContentChildren(self, g, s):
+        cookiesNums = len(s)
+        if cookiesNums == 0:
+            return 0
+        g.sort()
+        s.sort()
+
+        maxNum = 0
+        cookieIndex = cookiesNums - 1
+        childIndex = len(g) - 1
+        while cookieIndex >= 0 and childIndex >= 0:
+            if s[cookieIndex] >= g[childIndex]:
+                maxNum += 1
+                cookieIndex -= 1
+                childIndex -= 1
+            else:
+                childIndex -= 1
         
