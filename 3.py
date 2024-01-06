00d369fbfd83b1fe147b27c0b2753c1828c5518b
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2024-01-06 16:54:55 +0300

    Add files via upload

--- 3.py
+++ 3.py
@@ -1680,3 +1680,16 @@ class Solution(object):
                 if nums[i] > nums[j]:
                     arr[i] = max(arr[i], arr[j] + 1)
         return max(arr)
+    
+
+
+#!https://leetcode.com/problems/check-if-it-is-a-straight-line/
+class Solution(object):
+    def checkStraightLine(self, coordinates):
+        x0,y0=coordinates[0]
+        x1,y1=coordinates[1]
+        for x,y in coordinates[2:]:
+            if (y1-y0)*(x-x0)!=(y-y0)*(x1-x0):
+                return False
+
+        return True    
