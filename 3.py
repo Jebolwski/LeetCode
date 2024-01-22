bff0db5286ecb023bd881a7ae33e64f2738919c7
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2024-01-22 15:54:27 +0300

    Add files via upload

--- 3.py
+++ 3.py
@@ -1804,3 +1804,27 @@ class Solution(object):
                     if j-i+1>maxi_gomez:
                         maxi_gomez=j-i+1
         return maxi_gomez
+    
+
+#!https://leetcode.com/problems/subtree-of-another-tree/
+class Solution(object):
+    def isSubtree(self, root, subRoot):
+        """
+        :type root: TreeNode
+        :type subRoot: TreeNode
+        :rtype: bool
+        """ 
+        if root is None and subRoot is None:
+            return True
+        if subRoot is None:
+            return True
+        if root is None and subRoot is not None:
+            return False
+        return self.isSame(root,subRoot) or self.isSubtree(root.left,subRoot) or self.isSubtree(root.right,subRoot)
+
+    def isSame(self,root,subRoot):
+        if root is None and subRoot is None:
+            return True
+        if root is None or subRoot is None:
+            return False
+        return root.val == subRoot.val and self.isSame(root.left,subRoot.left) and self.isSame(root.right,subRoot.right)
