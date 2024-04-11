8c4f31d85eb6cfc90974dd0f19f974f99ce3b0ea
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2024-04-11 13:50:42 +0300

    Add files via upload

--- 3.py
+++ 3.py
@@ -2169,3 +2169,21 @@ class Solution(object):
                 return x+1
         return x+1
         
+#!https://leetcode.com/problems/remove-k-digits/
+class Solution(object):
+    def removeKdigits(self, num, k):
+        stk = []
+        ans = ""
+        for c in num:
+            while k > 0 and stk and stk[-1] > c:
+                stk.pop()
+                k -= 1
+            stk.append(c)
+        while k > 0:
+            stk.pop()
+            k -= 1
+        for c in stk:
+            if not ans and c == '0':
+                continue
+            ans += c
+        return ans if ans else '0'
