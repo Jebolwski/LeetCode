0d5f11a8e9832c840c003fa54258762b976204b0
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2024-06-30 14:57:28 +0300

    Add files via upload

--- 3.py
+++ 3.py
@@ -2689,3 +2689,18 @@ class Solution:
             if count == k:
                 return  key
         return ""
+
+#!https://leetcode.com/problems/get-maximum-in-generated-array/
+class Solution:
+    def getMaximumGenerated(self, n: int) -> int:
+        arr=[0,1]
+        if n==0:
+            return 0
+        for i in range(n-1):
+            arr.append(0)
+        for i in range(2,n+1):
+            if i%2==0:
+                arr[i]=arr[int(i/2)]
+            else:
+                arr[i]=arr[int(i/2)]+arr[int(i/2)+1]
+        return max(arr)
