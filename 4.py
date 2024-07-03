bbd0eae5be77ccd9ee2ff4a6a228bec4282360d7
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2024-07-03 12:35:27 +0300

    Add files via upload

--- 4.py
+++ 4.py
@@ -13,3 +13,14 @@ class Solution(object):
             elif (i+1)%3==0 or (i-1)%3==0:
                 x+=1
         return x
+
+#!https://leetcode.com/problems/difference-between-element-sum-and-digit-sum-of-an-array/   
+class Solution:
+    def differenceOfSum(self, nums: List[int]) -> int:
+        x=0
+        y=0
+        for i in nums:
+            x+=i
+            for j in str(i):
+                y+=int(j)
+        return abs(x-y)
