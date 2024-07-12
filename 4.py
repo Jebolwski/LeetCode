5ecff1cf43878b8b8b3fe13830c327f3ddf35ae6
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2024-07-12 22:17:27 +0300

    Add files via upload

--- 4.py
+++ 4.py
@@ -86,5 +86,31 @@ class Solution(object):
             time+=i[1]
             x+=(time-i[0])
         return x*1.00/len(customers)
+    
+#!https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses
+class Solution(object):
+    def __init__(self):
+        self.i = 0
+
+    def reverseParentheses(self, s):
+        def helper(s):
+            sb = []
+
+            while self.i < len(s):
+                if s[self.i] == ')':
+                    self.i += 1
+                    return ''.join(sb)[::-1]
+                elif s[self.i] == '(':
+                    self.i += 1
+                    st = helper(s)
+                    sb.append(st)
+                else:
+                    sb.append(s[self.i])
+                    self.i += 1
+
+            return ''.join(sb)
+
+        return helper(s)
+        
 
         
