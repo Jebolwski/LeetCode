3baea66b3aec4db06865270d179b626010c7dee7
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2024-07-19 23:34:35 +0300

    Add files via upload

--- 4.py
+++ 4.py
@@ -169,6 +169,23 @@ class Solution(object):
                 last_biggest=a^b
                 res=i
         return last_biggest
+
+#!https://leetcode.com/problems/number-of-good-leaf-nodes-pairs/
+class Solution:
+    def countPairs(self, root: TreeNode, distance: int) -> list:
+        self.res = 0  
+        
+        def dfs(node) -> list:
+            if not node: return []
+            if not node.left and not node.right: return [1]
+
+            left_list = dfs(node.left)
+            right_list = dfs(node.right)
+            self.res += sum(l+r <= distance for l in left_list for r in right_list)
+            return [1+item for item in left_list+right_list]
+        
+        dfs(root)
+        return self.res 
         
 
         
