59ecf4e22087b006e88cd36575d7768e221f8281
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2024-08-08 23:18:05 +0300

    Add files via upload

--- 4.py
+++ 4.py
@@ -1,4 +1,4 @@
-#!https://leetcode.com/problems/find-the-maximum-achievable-number
+#!https://leetcode.com/problems/find-the-maximum-achievable-number/
 class Solution(object):
     def theMaximumAchievableX(self, num, t):
         return num+t*2
@@ -275,6 +275,24 @@ class Solution:
             mx = max(mx, cnt)
         return k - mx
 
+#!https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/
+class Solution:
+    def minimumPushes(self, input_text: str) -> int:
+        # Count letter occurrences
+        letter_counts = [0] * 26
+        for char in input_text:
+            letter_counts[ord(char) - ord('a')] += 1
+        
+        # Sort counts in descending order
+        sorted_counts = sorted(letter_counts, reverse=True)
+        
+        total_key_presses = 0
+        for index, count in enumerate(sorted_counts):
+            if count == 0:
+                break
+            total_key_presses += (index // 8 + 1) * count
+        
+        return total_key_presses
         
 
         
