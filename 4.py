ad305cb502d15db410ce2d8561ade21d77f08310
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2024-08-13 23:29:33 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,4 +1,4 @@
-#!https://leetcode.com/problems/find-the-maximum-achievable-number/
+#!https://leetcode.com/problems/find-the-maximum-achievable-number
 class Solution(object):
     def theMaximumAchievableX(self, num, t):
         return num+t*2
