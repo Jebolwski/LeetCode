32882f7b6ff86c4401e2806fcfef03aef87881b3
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2024-08-18 12:12:58 +0300

    Add files via upload

--- 4.py
+++ 4.py
@@ -298,6 +298,23 @@ class Solution:
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
         return sum(abs(x-y) for x, y in zip(sorted(seats), sorted(students)))
+
+#!https://leetcode.com/problems/ugly-number-ii/ 
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        ugly=[1]
+        two=three=five=0
+        
+        while len(ugly)<n:
+            while ugly[two]*2<=ugly[-1]: two+=1
+            
+            while ugly[three]*3<=ugly[-1]:three+=1
+                
+            while ugly[five]*5<=ugly[-1]:five+=1
+                
+            ugly.append(min(ugly[two]*2,ugly[three]*3,ugly[five]*5))
+            
+        return ugly[-1]  
         
 
         
