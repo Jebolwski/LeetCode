22709252a8bb63d76dd09f613ed5c2d8b1632e45
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2024-08-20 16:27:26 +0300

    Add files via upload

--- 1.js
+++ 1.js
@@ -143,3 +143,21 @@ var isEmpty = function (obj) {
   }
   return false;
 };
+
+//!https://leetcode.com/problems/flatten-deeply-nested-array/
+var flat = function (arr, n) {
+  const stack = [...arr.map((item) => [item, n])];
+  const result = [];
+
+  while (stack.length > 0) {
+    const [item, n] = stack.pop();
+
+    if (Array.isArray(item) && n > 0) {
+      stack.push(...item.map((subItem) => [subItem, n - 1]));
+    } else {
+      result.push(item);
+    }
+  }
+
+  return result.reverse();
+};
