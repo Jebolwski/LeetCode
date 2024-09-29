8c92fcc96a0b11d0d6bb2142e5b8f40a3e780962
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2024-09-29 23:12:50 +0300

    Add files via upload

--- 4.py
+++ 4.py
@@ -694,3 +694,24 @@ class Solution(object):
             if s==("".join(words[:i])):
                 return True
         return False
+
+#!https://leetcode.com/problems/partition-array-into-three-parts-with-equal-sum/
+class Solution(object):
+    def canThreePartsEqualSum(self, arr):
+        total_sum = sum(arr)
+        if total_sum % 3 != 0:
+            return False
+
+        target = total_sum // 3
+        current_sum = 0
+        count = 0
+
+        for i in range(len(arr) - 1):
+            current_sum += arr[i]
+            if current_sum == target:
+                current_sum = 0
+                count += 1
+                if count == 2:
+                    return True
+
+        return False
