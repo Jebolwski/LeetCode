09974c2be52cee3eb81db491aefd19950d982a8b
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2024-09-30 18:45:15 +0300

    Add files via upload

--- 4.py
+++ 4.py
@@ -715,3 +715,31 @@ class Solution(object):
                     return True
 
         return False
+    
+#!https://leetcode.com/problems/design-a-stack-with-increment-operation/
+class CustomStack(object):
+
+    def __init__(self, maxSize):
+        self.stack=[]
+        self.maxSize=maxSize
+        
+
+    def push(self, x):
+        if len(self.stack)<self.maxSize:
+            self.stack.append(x)
+
+
+    def pop(self):
+        if len(self.stack)==0:
+            return -1
+        return self.stack.pop()
+
+    def increment(self, k, val):
+        if len(self.stack)>=k:
+            for i in range(k):
+                self.stack[i]+=val
+        else:
+            for i in range(len(self.stack)):
+                self.stack[i]+=val
+
+
