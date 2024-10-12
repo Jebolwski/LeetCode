dbe9eacf4407380d17a03b2863c5fac32b3171b9
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2024-10-12 14:38:34 +0300

    Add files via upload

--- 4.py
+++ 4.py
@@ -918,3 +918,18 @@ class Solution(object):
             if x>res:
                 res=x
         return res
+
+#!https://leetcode.com/problems/divide-intervals-into-minimum-number-of-groups/
+class Solution:
+    def minGroups(self, intervals: List[List[int]]) -> int:
+        start_times = sorted(i[0] for i in intervals)
+        end_times = sorted(i[1] for i in intervals)
+        end_ptr, group_count = 0, 0
+
+        for start in start_times:
+            if start > end_times[end_ptr]:
+                end_ptr += 1
+            else:
+                group_count += 1
+
+        return group_count
