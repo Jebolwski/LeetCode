bbde5c1e260910eca8527095bd3c10497f9dbc1e
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2024-11-03 13:55:52 +0300

    Add files via upload

--- 4.py
+++ 4.py
@@ -1105,3 +1105,15 @@ class Solution(object):
             if i!="":
                 res.append(i)
         return res
+
+#!https://leetcode.com/problems/circular-sentence/ 
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        sentence = sentence.split(" ")
+        if len(sentence)==1:
+            return sentence[0][0]==sentence[0][-1]
+        
+        for i in range(len(sentence)-1):
+            if sentence[i][-1]!=sentence[i+1][0]:
+                return False
+        return sentence[0][0]==sentence[-1][-1]
