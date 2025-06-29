84ee8c4b14c1cbfc953d17bcc2973934d68e6d0c
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2025-06-29 22:41:12 +0300

    Add files via upload

--- 4.py
+++ 4.py
@@ -1379,3 +1379,25 @@ class Solution:
         dicts[maxLen].sort()
         
         return dicts[maxLen][len(dicts[maxLen]) - 1]
+
+#!https://leetcode.com/problems/count-vowel-substrings-of-a-string/
+class Solution:
+    def countVowelSubstrings(self, word: str) -> int:
+        count = 0 
+        current = set() 
+        for i in range(len(word)):
+            if word[i] in 'aeiou':
+                current.add(word[i])
+                
+                for j in range(i+1, len(word)): 
+                    if word[j] in 'aeiou':
+                        current.add(word[j])
+                    else:
+                        break
+                    
+                    if len(current) == 5:
+                        count += 1
+						
+            current = set()
+                        
+        return count
