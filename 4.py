085a0c9653eb9008260a9803b40c852221eea109
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2025-07-12 16:43:09 +0300

    Add files via upload

--- 4.py
+++ 4.py
@@ -1401,3 +1401,26 @@ class Solution:
             current = set()
                         
         return count
+
+#!https://leetcode.com/problems/merge-two-2d-arrays-by-summing-values/
+class Solution:
+    def mergeArrays(self, nums1, nums2):
+        result = []
+        i, j = 0, 0
+
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i][0] == nums2[j][0]:
+                result.append([nums1[i][0], nums1[i][1] + nums2[j][1]])
+                i += 1
+                j += 1
+            elif nums1[i][0] < nums2[j][0]:
+                result.append(nums1[i])
+                i += 1
+            else:
+                result.append(nums2[j])
+                j += 1
+
+        result.extend(nums1[i:])
+        result.extend(nums2[j:])
+
+        return result
