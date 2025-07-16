c17269a022c05539dfd5e542dc78776d46ca4c0d
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2025-07-16 16:30:13 +0300

    Add files via upload

--- 4.py
+++ 4.py
@@ -1447,3 +1447,26 @@ class Solution(object):
             if lower(i) not in alphabet and lower(i) not in vowels and i not in digits:
                 return False
         return True
+
+#!https://leetcode.com/problems/remove-one-element-to-make-the-array-strictly-increasing/
+class Solution:
+    def canBeIncreasing(self, nums):
+        indx = -1
+        count = 0
+        n = len(nums)
+        
+        for i in range(n-1):
+            if nums[i] >= nums[i+1]:
+                indx = i
+                count += 1
+        
+        if count==0:
+            return True
+        
+        if count == 1:
+            if indx == 0 or indx == n-2:
+                return True
+            if nums[indx-1] < nums[indx+1] or(indx+2 < n and nums[indx] < nums[indx+2]):
+                return True
+            
+        return False
