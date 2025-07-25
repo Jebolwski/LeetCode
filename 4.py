315d9f2b0ac45563ba377d8ba7ce1d740c4c1c60
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2025-07-25 23:45:42 +0300

    Add files via upload

--- 4.py
+++ 4.py
@@ -1470,3 +1470,18 @@ class Solution:
                 return True
             
         return False
+
+
+#!https://leetcode.com/problems/maximum-unique-subarray-sum-after-deletion/
+class Solution:
+    def maxSum(self, nums: list[int]) -> int:
+        max_value = max(nums)
+        if all(n < 0 for n in nums):
+            return max_value
+
+        seen = [False] * 101
+        for n in nums:
+            if 0 <= n <= 100:
+                seen[n] = True
+
+        return sum(i for i, present in enumerate(seen[1:], start=1) if present)
