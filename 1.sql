945a9def46f016fce8965ec719504466f88cadb6
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2026-02-12 12:19:05 +0300

    Add files via upload

--- 1.sql
+++ 1.sql
@@ -527,3 +527,11 @@ HAVING
 select score, 
 dense_rank() over (order by score desc) as 'rank'
 from Scores order by score desc
+
+--!https://leetcode.com/problems/confirmation-rate/
+SELECT 
+    s.user_id,
+    ROUND(AVG(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END), 2) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c ON s.user_id = c.user_id
+GROUP BY s.user_id;
