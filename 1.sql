152aed14fdec82b4d8c2c68f1f200cdf1f12d1a6
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2026-02-13 14:24:53 +0300

    Add files via upload

--- 1.sql
+++ 1.sql
@@ -535,3 +535,20 @@ SELECT
 FROM Signups s
 LEFT JOIN Confirmations c ON s.user_id = c.user_id
 GROUP BY s.user_id;
+
+--!https://leetcode.com/problems/department-top-three-salaries/
+SELECT 
+    d.name AS Department, 
+    e.name AS Employee, 
+    e.salary AS Salary
+FROM 
+    Employee e
+JOIN 
+    Department d ON e.departmentId = d.id
+WHERE 
+    (SELECT COUNT(DISTINCT e2.salary) 
+     FROM Employee e2 
+     WHERE e2.departmentId = e.departmentId 
+     AND e2.salary > e.salary) < 3
+ORDER BY 
+    Department, Salary DESC;
